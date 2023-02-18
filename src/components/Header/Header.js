7f51d8e0af11588cce5381fb4d8303c16393ff4c
Rajnish <rajnishtripathi2001@gmail.com>
2023-02-18 20:55:13 +0530

    header static text and its styling added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
   return (
-    <section className='header'>
-      
-      <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-          <source src="vcBgFootage.mp4" type="video/mp4"/>
+    <section className="header">
+      <video
+        playsinline="playsinline"
+        autoplay="autoplay"
+        muted="muted"
+        loop="loop"
+      >
+        <source src="vcBgFootage.mp4" type="video/mp4" />
       </video>
 
       <div className="overlay">
         <div className="col1"></div>
+
         <div className="col2">
-        <h1>hello world</h1>
-        <p>omg</p>
+          <div className="population">
+            <h3 className="population-count">102598</h3>
+            <p>POPULATION OF VINE CITY</p>
+          </div>
+          <div className="heading">
+            <h1>The Virtual Nation</h1>
+            <p>Build the future through democracy and innovations.</p>
+            <p>No matter where you are.</p>
+          </div>
+          <div className="join-button-container">
+            <button className="join-button">BECOME A VINISH</button>
+          </div>
         </div>
-        <div className="col3"></div>
+        
+        <div className="col1"></div>
       </div>
-
-      
-
-    
-
-      
     </section>
-  )
+  );
 }
