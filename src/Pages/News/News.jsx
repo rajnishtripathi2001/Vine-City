9d487075ee5cc33d9c2d11697e0af1cda336a021
Rajnish <rajnishtripathi2001@gmail.com>
2023-04-20 01:10:19 +0530

    news page updated

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import './News.css'
-import PageNavBar from '../../components/PageNavBar/PageNavBar'
-import Footer from '../../components/Footer/Footer'
+import React from "react";
+import "./News.css";
+import PageNavBar from "../../components/PageNavBar/PageNavBar";
+import Footer from "../../components/Footer/Footer";
 
 export default function News() {
   return (
     <>
-    <PageNavBar/>
-    <div className='news-body'>
-    this is news page
-    </div>
-    <Footer/>
+      <PageNavBar />
+      <div className="news-body">
+        <b>
+          We're excited to let you know that we're currently working on our Social page <br />
+          to bring you an even better experience! We apologize for the inconvenience <br /> and kindly  
+          ask you to check back soon. <br />
+          We're looking forward to sharing the updates with you. <br /> Thank you for your
+          understanding and patience.
+        </b>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
