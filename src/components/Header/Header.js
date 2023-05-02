b87141fa10c829135c2f1f75716cb548c95e17f1
Rajnish <rajnishtripathi2001@gmail.com>
2023-05-02 23:26:34 +0530

    header and navbar are resposive now

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,10 @@
 import "./Header.css";
 import { React } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-// import required modules
 import { Pagination, Navigation } from "swiper";
-
 
 export default function Header({ counter }) {
   
